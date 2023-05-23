c0e099615de4afa7a3c150a98dd0d9bf3535d7ab
Anindia Azzahra <123210072@student.upnyk.ac.id>
2023-05-24 01:43:13 +0700

    Add tutorial

--- src/view/TutorialView.java
+++ src/view/TutorialView.java
@@ -3,7 +3,6 @@ package view;
 import controller.TutorialController;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Image;
@@ -13,20 +12,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import controller.LeaderboardData;
-import model.LeaderboardModel;
-import javax.swing.BorderFactory;
-import javax.swing.border.Border;
 
 public class TutorialView extends JFrame {
     
     public static final int LEBAR = 600;
     public static final int TINGGI = 706;
-    public JButton backBtn,deleteBtn;
-    private JLabel titleLabel;
+    public JButton backBtn, deleteBtn;
+    private JLabel titleLabel, arrowKeyLabel, spaceKeyLabel;
     private JPanel tutorialPanel;
     private JTable leaderboardTable;
     private DefaultTableModel tableModel;
@@ -37,15 +31,15 @@ public class TutorialView extends JFrame {
         setPreferredSize(new Dimension(LEBAR, TINGGI));
         setBackground(new Color(38, 38, 38));
 
-        JPanel tutorialPanel = new JPanel(new FlowLayout());
+        tutorialPanel = new JPanel();
         
-        ImageIcon backIcon = new ImageIcon(getClass().getResource("/resources/icons/left-arrow.png"));
+        ImageIcon backIcon = new ImageIcon(getClass().getResource("/resources/icons/arrow.png"));
         ImageIcon originalIcon = backIcon;
 
         Image image = originalIcon.getImage();
 
-        int newWidth = 50;
-        int newHeight = 50;
+        int newWidth = 34;
+        int newHeight = 30;
 
         Image resizedImage = image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
@@ -63,39 +57,51 @@ public class TutorialView extends JFrame {
         Font font = customFont.deriveFont(Font.PLAIN, 28);
         titleLabel.setFont(font);
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        titleLabel.setBounds(0, 100, 210, 50);
         titleLabel.setForeground(Color.WHITE);
         
-        backBtn.setBounds(80, 500, 50, 50);
         
         tutorialPanel.add(backBtn);
         tutorialPanel.add(titleLabel);
         tutorialPanel.setBackground(new Color(38, 38, 38));
-
-
+        tutorialPanel.setLayout(null);
         
-     
-
-
+        backBtn.setBounds(20, 20, 40, 40);
+        titleLabel.setBounds(100, 80, 400, 50);
         
-        // setContentPane(leaderboardPanel);
+        // set label w icon
+        ImageIcon arrowKey = new ImageIcon(getClass().getResource("/resources/icons/arrow-key-img.jpg"));
+        Image arrowKeyImage = arrowKey.getImage();
+        
+        newWidth = 212; 
+        newHeight = 222; 
+        
+        resizedImage = arrowKeyImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        resizedIcon = new ImageIcon(resizedImage);
+        
+        arrowKeyLabel = new JLabel(resizedIcon);
+        
+        
+        ImageIcon spaceKey = new ImageIcon(getClass().getResource("/resources/icons/space-img.jpg"));
+        Image spaceKeyImage = spaceKey.getImage();
+
+        newWidth = 262; 
+        newHeight = 110; 
+
+        resizedImage = spaceKeyImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        resizedIcon = new ImageIcon(resizedImage);
+
+        spaceKeyLabel = new JLabel(resizedIcon);
+        
+
+        tutorialPanel.add(arrowKeyLabel);
+        arrowKeyLabel.setBounds(180, 180, 212, 222);
+        tutorialPanel.add(spaceKeyLabel);
+        spaceKeyLabel.setBounds(160, 460, 262, 110);
         add(tutorialPanel);
         
         pack();
         setLocationRelativeTo(null);
         
         TutorialController tutorialController = new TutorialController(this);
-    }
-
-    public DefaultTableModel getTableModel() {
-        return tableModel;
-    }
-
-    public JTable getLeaderboardTable() {
-        return leaderboardTable;
-    }
-    
-    
-    
-    
+    }        
 }
